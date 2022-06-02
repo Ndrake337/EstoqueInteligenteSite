3b1a431805715155ebed7b4d642613174348fefb
Matheus <matheus.felipe337@gmail.com>
2022-06-02 15:38:59 -0300

    Dashboard Consumindo dados do backen

--- src/pages/CurrentState/CurrentState.jsx
+++ src/pages/CurrentState/CurrentState.jsx
@@ -1,13 +1,33 @@
 import "./CurrentState.css"
 import MonitorWeightIcon from '@mui/icons-material/MonitorWeight';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 export default function CurrentState({ data }) {
-  const listDevices = data.map((data) =>
-    <div className="currentStateData" key={data}>
-      <span className="slot">{data.name}</span>
+  const [CurrentState, setCurrentState] = useState([])
+
+  // fetches data
+  async function fetchData() {
+    await Axios.get("https://apiestoqueinteligente.herokuapp.com/prateleiras").then((response) => {
+      setCurrentState(response.data);
+      console.log(response.data);
+    })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  const listDevices = CurrentState.map((CurrentState) =>
+    <div className="currentStateData" key={CurrentState}>
+      <span className="slot">Slot {CurrentState.id_prateleira}</span>
       <MonitorWeightIcon className="WeightIcon" style={{ fontSize: 500 }} />
-      <span className="title">Quantidade de Produtos: {data.amountOfProducts} Unidades</span>
-      <span className="updatedDate">Ultima atualização em: {data.last_Update}</span>
+      <span className="title">Produto Estocado: {CurrentState.nome_produto} </span>
+      <span className="title">Quantidade de Produtos: {CurrentState.quantidade} Unidade(s)</span>
+      <span className="updatedDate">Ultima atualização em: {CurrentState.data_Alteracao}</span>
     </div>
   );
   return (
