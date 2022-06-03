0468af3286e33494919dfd3ecfef972d43f7364f
Matheus <matheus.felipe337@gmail.com>
2022-06-03 18:14:40 -0300

    Logo de raposinha

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -5,8 +5,8 @@ export default function TopBar() {
   return (
     <div className='TopBar'>
       <div className='TopBarContainer'>
-        <div className='TopBarIcon'>
-          <img src='logo192.png' alt='Logo' className='logo' href='/' />
+        <div className='TopBarIcon'> 
+        <img src='logo.png' alt='Logo' className='logo' href='/#' width='80%' height='80%' />
         </div>
         <div className='MenuBar'>
           <a href='/'> Home </a>
